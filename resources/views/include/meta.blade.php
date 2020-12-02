9d6a2bc3abf37901dce207058a2fef4451dd848f
Radityo Putra <radityo.putra@gmail.com>
2020-12-02 13:07:49 +0700

    modif csp3

--- resources/views/include/meta.blade.php
+++ resources/views/include/meta.blade.php
@@ -1,9 +1,5 @@
 <meta http-equiv="X-UA-Compatible" content="chrome=1">
-<meta http-equiv="Content-Security-Policy"
-    content="
-      child-src blob: gap:;
-      img-src 'self' blob: data:;
-      default-src * 'self' 'unsafe-inline' 'unsafe-eval' data: gap: content:">
+<meta http-equiv="Content-Security-Policy" content="default-src *; style-src 'self' http://* 'unsafe-inline'; script-src 'self' http://* 'unsafe-inline' 'unsafe-eval'" />
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
 <meta name="author" content="signfinger.com">
